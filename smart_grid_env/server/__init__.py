74ea5b9bdb0b27bb316f4abf80460f81699f86eb
Rakshit Chaturvedi <rakshitchaturvedi09@gmail.com>
2026-04-06 20:27:24 +0530

    corrected function names in init

--- smart_grid_env/server/__init__.py
+++ smart_grid_env/server/__init__.py
@@ -1,3 +1,3 @@
 from .grid_env import SmartGridEnv
 from .simulator import GridSimulator
-from .tasks import EasyTask, MediumTask, HardTask
+from .tasks import PeakSurvivalTask, DailyBalanceTask, ExtremeWeatherTask
